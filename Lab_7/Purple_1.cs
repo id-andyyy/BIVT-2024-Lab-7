8899922cdc2dfd011e91a76f6f04c74fe4a0af3e
Andy <obrezkov.andy@gmail.com>
2025-03-07 14:29:29 +0300

    Add Purple_1

---
+++ Lab_7/Purple_1.cs
@@ -0,0 +1,203 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class Purple_1
+    {
+        public class Participant
+        {
+            private string _name;
+            private string _surname;
+            private double[] _coefs;
+            private int[,] _marks;
+
+            private int _curJump;
+
+            public string Name => _name;
+            public string Surname => _surname;
+
+            public double[] Coefs => (double[])_coefs?.Clone();
+            public int[,] Marks => (int[,])_marks?.Clone();
+
+            public double TotalScore { get; private set; }
+
+            public Participant(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _coefs = new double[] { 2.5, 2.5, 2.5, 2.5 };
+                _marks = new int[4, 7];
+                TotalScore = 0;
+
+                _curJump = 0;
+            }
+
+            public void SetCriterias(double[] coefs)
+            {
+                if (coefs == null || coefs.Length != 4 || _coefs == null) return;
+
+                _coefs = (double[])coefs.Clone();
+            }
+
+            public void Jump(int[] marks)
+            {
+                if (_curJump >= 4 || marks == null || marks.Length != 7 || _marks == null || _coefs == null) return;
+
+                for (int j = 0; j < marks.Length; j++)
+                {
+                    _marks[_curJump, j] = marks[j];
+                }
+
+                TotalScore += (marks.Sum() - marks.Max() - marks.Min()) * _coefs[_curJump];
+                _curJump++;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"Имя: {_name ?? "не задано"}");
+                Console.WriteLine($"Фамилия: {_surname ?? "не задано"}");
+
+                Console.Write("Коэффициенты:\t");
+                if (_coefs == null) Console.Write("не заданы");
+                else
+                {
+                    foreach (var coef in _coefs)
+                    {
+                        Console.Write($"{coef}\t");
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Console.Write("Оценки:");
+                if (_marks == null) Console.Write("не заданы");
+                else
+                {
+                    for (int i = 0; i < _marks.GetLength(0); i++)
+                    {
+                        Console.Write("\t");
+                        for (int j = 0; j < _marks.GetLength(1); j++)
+                        {
+                            Console.Write($"{_marks[i, j]}\t");
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+
+                Console.WriteLine($"Результат: {Math.Round(TotalScore, 2)}");
+                Console.WriteLine();
+            }
+
+            public static void Sort(Participant[] array)
+            {
+                if (array == null) return;
+
+                var sortedArray = array
+                    .Where(x => x != null)
+                    .OrderByDescending(x => x.TotalScore)
+                    .Concat(array.Where(x => x == null))
+                    .ToArray();
+                Array.Copy(sortedArray, array, sortedArray.Length);
+            }
+        }
+
+        public class Judge
+        {
+            private string _name;
+            private int[] _favouriteMarks;
+            private int _curMark;
+
+            public string Name => _name;
+
+            public Judge(string name, int[] favouriteMarks)
+            {
+                _name = name;
+                _favouriteMarks = (int[])favouriteMarks?.Clone();
+
+                _curMark = 0;
+            }
+
+            public int CreateMark()
+            {
+                if (_favouriteMarks == null || _favouriteMarks.Length == 0) return 0;
+
+                if (_curMark == _favouriteMarks.Length) _curMark = 0;
+                return _favouriteMarks[_curMark++];
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"Имя: {_name ?? "не задано"}");
+
+                Console.Write("Любимые оценки:\t");
+                if (_favouriteMarks == null) Console.Write("не заданы");
+                else
+                {
+                    foreach (var mark in _favouriteMarks)
+                    {
+                        Console.Write($"{mark}\t");
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public class Competition
+        {
+            private Judge[] _judges;
+            private Participant[] _participants;
+
+            public Judge[] Judges => (Judge[])_judges?.Clone();
+            public Participant[] Participants => (Participant[])_participants?.Clone();
+
+            public Competition(Judge[] judges)
+            {
+                _judges = (Judge[])judges?.Clone();
+                _participants = new Participant[0];
+            }
+
+            public void Evaluate(Participant jumper)
+            {
+                if (_judges == null || jumper == null) return;
+
+                jumper.Jump(_judges.Select(x => x == null ? 0 : x.CreateMark()).ToArray());
+            }
+
+            public void Add(Participant participant)
+            {
+                if (_participants == null) return;
+
+                Array.Resize(ref _participants, _participants.Length + 1);
+                _participants[_participants.Length - 1] = participant;
+                Evaluate(_participants[_participants.Length - 1]);
+            }
+
+            public void Add(Participant[] participants)
+            {
+                if (_participants == null || participants == null) return;
+
+                int n = _participants.Length;
+                _participants = _participants.Concat(participants).ToArray();
+
+                for (int i = n; i < _participants.Length; i++)
+                {
+                    Evaluate(_participants[i]);
+                }
+            }
+
+            public void Sort()
+            {
+                if (_participants == null) return;
+
+                Participant.Sort(_participants);
+            }
+        }
+    }
+}
--- Lab_7/obj/Debug/net8.0/Lab_7.AssemblyInfo.cs
+++ Lab_7/obj/Debug/net8.0/Lab_7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a1f8499f020dcd9c9c5254b15f4e97542e9e7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+677e9195d5c12f5709d0f42280b819d2cb5e4b55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
